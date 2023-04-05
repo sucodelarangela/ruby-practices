af27b3d1bdd5fbca44704fb71cf3f37781f2a64b
Angela Caldas <aes.caldas@gmail.com>
2023-04-05 13:19:03 -0300

    wip: nested collections

---
+++ 12-nested-collections.rb
@@ -0,0 +1,58 @@
+# NESTED ARRAYS
+test_scores = [
+  [97, 76, 79, 93],
+  [79, 84, 76, 79],
+  [88, 67, 64, 76],
+  [94, 55, 67, 81],
+]
+
+teacher_mailboxes = [
+  ["Adams", "Baker", "Clark", "Davis"],
+  ["Jones", "Lewis", "Lopez", "Moore"],
+  ["Perez", "Scott", "Smith", "Young"]
+]
+
+# Acessando elementos
+teacher_mailboxes[0][0] # "Adams"
+teacher_mailboxes[1][0] # "Jones"
+teacher_mailboxes[2][0] # "Perez"
+
+# Índices negativos retornam elementos do final do array, começando em [-1]
+teacher_mailboxes[0][-1] # "Davis"
+teacher_mailboxes[-1][0] # "Perez"
+teacher_mailboxes[-1][-2] # "Smith"
+
+# Acessar índices de elementos aninhados não existentes gera um `NoMethodError`, porque a classe `nil` não tem um método []
+teacher_mailboxes[3][0] # NoMethodError pois só temos até o índice 2
+
+# Acessar um índice inexistente dentro de um elemento aninhado existente, retorna `nil`
+teacher_mailboxes[0][4] # nil
+
+# Para retornar `nil` nos casos acima, utilizamos o método `dig`
+teacher_mailboxes.dig(3, 0) # nil
+teacher_mailboxes.dig(0, 4) # nil
+
+# CRIANDO NESTED ARRAYS
+# Criar arrays com o método `Array.new(initial size, default value)` só aceita como segundo argumento objetos imutáveis, como `number`, `boolean` ou `symbol`. Se usarmos string, array, hash ou outro objeto mutável faz com que todos os elementos do array sejam mudados quando você muda apenas um dos elementos
+mutable = Array.new(3, Array.new(2)) # [[nil, nil], [nil, nil], [nil, nil]]
+mutable[0][0] = 1000
+mutable # [[1000, nil], [1000, nil], [1000, nil]]
+
+# Sendo assim, devemos criar um nested array omitindo o segundo argumento opcional e passando-o dentro de um bloco {}:
+immutable = Array.new(3) { Array.new(2) } # [[nil, nil], [nil, nil], [nil, nil]]
+immutable[0][0] = 1000
+immutable # [[1000, nil], [nil, nil], [nil, nil]]
+
+# ADICIONANDO E REMOVENDO ELEMENTOS
+# Podemos adicionar usando o método `push` ou o shovel operator `<<`. Para adicionarmos em um nested array, devemos indicar o índice do mesmo:
+test_scores << [1000, 99, 98, 97]
+# [[97, 76, 79, 93], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81], [100, 99, 98, 97]]
+test_scores[0].push(100) # [97, 76, 79, 93, 100]
+test_scores
+# [[97, 76, 79, 93, 100], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81], [100, 99, 98, 97]]
+
+# Usando sintaxe similar, podemos remover elementos com `pop`
+test_scores.pop # [100, 99, 98, 97]
+test_scores[0].pop # 100
+test_scores
+# [[97, 76, 79, 93], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81]]

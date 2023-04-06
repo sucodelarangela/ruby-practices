55bac621b9f83baa893f6f1a63b9d0ca35f8d6b8
Angela Caldas <aes.caldas@gmail.com>
2023-04-06 13:28:55 -0300

    nested collections (nested arrays)

--- 12-nested-collections.rb
+++ 12-nested-collections.rb
@@ -23,7 +23,7 @@ teacher_mailboxes[-1][0] # "Perez"
 teacher_mailboxes[-1][-2] # "Smith"
 
 # Acessar índices de elementos aninhados não existentes gera um `NoMethodError`, porque a classe `nil` não tem um método []
-teacher_mailboxes[3][0] # NoMethodError pois só temos até o índice 2
+# teacher_mailboxes[3][0] # NoMethodError pois só temos até o índice 2
 
 # Acessar um índice inexistente dentro de um elemento aninhado existente, retorna `nil`
 teacher_mailboxes[0][4] # nil
@@ -56,3 +56,59 @@ test_scores.pop # [100, 99, 98, 97]
 test_scores[0].pop # 100
 test_scores
 # [[97, 76, 79, 93], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81]]
+
+# ITERANDO EM UM NESTED ARRAY
+# Exemplo com `each_with_index`
+teacher_mailboxes.each_with_index do |row, row_index|
+  puts "Row: #{row_index} = #{row}"
+end
+# Row: 0 = ["Adams", "Baker", "Clark", "Davis"]
+# Row: 1 = ["Jones", "Lewis", "Lopez", "Moore"]
+# Row: 2 = ["Perez", "Scott", "Smith", "Young"]
+
+# Para conseguir iterar em elementos individuais em cada linha, você precisa aninhar outro método:
+teacher_mailboxes.each_with_index do |row, row_index|
+  row.each_with_index do |teacher, column_index|
+    puts "Row: #{row_index} Column: #{column_index} = #{teacher}"
+  end
+end
+# Row: 0 Column: 0 = Adams
+# Row: 0 Column: 1 = Baker
+# Row: 0 Column: 2 = Clark
+# Row: 0 Column: 3 = Davis
+# Row: 1 Column: 0 = Jones
+# Row: 1 Column: 1 = Lewis
+# Row: 1 Column: 2 = Lopez
+# Row: 1 Column: 3 = Moore
+# Row: 2 Column: 0 = Perez
+# Row: 2 Column: 1 = Scott
+# Row: 2 Column: 2 = Smith
+# Row: 2 Column: 3 = Young
+
+# Se precisarmos apenas do nome de cada professor, podemos usar o método `flatten` antes de iterar:
+teacher_mailboxes.flatten.each do |teacher|
+  puts "#{teacher} is amazing!"
+end
+# Adams is amazing!
+# Baker is amazing!
+# Clark is amazing!
+# Davis is amazing!
+# Jones is amazing!
+# Lewis is amazing!
+# Lopez is amazing!
+# Moore is amazing!
+# Perez is amazing!
+# Scott is amazing!
+# Smith is amazing!
+# Young is amazing!
+
+# EXEMPLO MAIS COMPLEXO
+test_scores.any? do |scores|
+  scores.all? { |score| score > 80 }
+end
+# O exemplo acima retorna `false` pois não temos nenhum nested array com todas as notas acima de 80
+
+test_scores.all? do |scores|
+  scores.any? { |score| score > 80 }
+end
+# O exemplo acima retorna `true` pois validamos que todos os nested arrays tem algum número acima de 80
